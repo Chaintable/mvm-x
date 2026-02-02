6f722bd083e36b1a0ef3ef3c705b7dfcf3e3dea1
Zhang Guyu <zhangguyu6@gmail.com>
2026-02-02 05:15:38 +0000

    fix

--- l2geth/core/state_processor.go
+++ l2geth/core/state_processor.go
@@ -24,6 +24,7 @@ import (
 	"github.com/MetisProtocol/mvm/l2geth/core/types"
 	"github.com/MetisProtocol/mvm/l2geth/core/vm"
 	"github.com/MetisProtocol/mvm/l2geth/crypto"
+	"github.com/MetisProtocol/mvm/l2geth/log"
 	"github.com/MetisProtocol/mvm/l2geth/params"
 	"github.com/MetisProtocol/mvm/l2geth/rollup/fees"
 	"github.com/MetisProtocol/mvm/l2geth/rollup/rcfg"
@@ -69,6 +70,7 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 	}
 	// Iterate over and process the individual transactions
 	for i, tx := range block.Transactions() {
+		log.Info("Processing transaction", "chainid", tx.ChainId())
 		if cfg.TracerExt != nil {
 			msg, err := tx.AsMessage(types.MakeSigner(p.config, header.Number))
 			if err != nil {
